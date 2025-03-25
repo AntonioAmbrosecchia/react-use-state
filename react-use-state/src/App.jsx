a55a1aef8f3f4ef8283e3e1305acad3d24822554
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-03-26 00:43:45 +0100

    add js, css, html

--- react-use-state/src/App.jsx
+++ react-use-state/src/App.jsx
@@ -1,21 +1,55 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import './App.css'; // Importa il file CSS
 
-function App() {
+const languages = [
+  { name: "HTML", description: "HTML è il linguaggio di markup per la creazione di pagine web." },
+  { name: "CSS", description: "CSS è il linguaggio per la stilizzazione delle pagine web." },
+  { name: "JavaScript", description: "JavaScript è un linguaggio di programmazione per la creazione di interazioni dinamiche sulle pagine web." },
+  { name: "React", description: "React è una libreria JavaScript per la creazione di interfacce utente." },
 
+];
+
+const LanguageButtons = ({ languages, setSelectedLanguage, selectedLanguage }) => {
+  return (
+    <div>
+      {languages.map((language, index) => (
+        <button
+          key={index}
+          onClick={() => setSelectedLanguage(language)}
+          className={`button ${language === selectedLanguage ? 'selected' : ''}`} // Aggiungi la classe 'selected' se il bottone è selezionato
+        >
+          {language.name}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+const LanguageCard = ({ language }) => {
+  return (
+    <div className="card">
+      {language ? (
+        <div>
+          <h2>{language.name}</h2>
+          <p>{language.description}</p>
+        </div>
+      ) : (
+        <p>Nessun linguaggio selezionato</p>
+      )}
+    </div>
+  );
+};
+
+const App = () => {
   const [selectedLanguage, setSelectedLanguage] = useState(null);
 
   return (
-    <div>
-      <h1>useState</h1>
+    <div className="container">
+      <h1>Learn Web development</h1>
+      <LanguageButtons languages={languages} setSelectedLanguage={setSelectedLanguage} selectedLanguage={selectedLanguage} />
+      <LanguageCard language={selectedLanguage} />
     </div>
   );
-}
-
-const languages = [
-  { name: "HTML", description: "Linguaggio di markup per il web." },
-  { name: "CSS", description: "Foglio di stile per la presentazione delle pagine web." },
-  { name: "JavaScript", description: "Linguaggio di programmazione per il web." },
-];
-
+};
 
 export default App;
